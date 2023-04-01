b29c1a07e5ac1eb7abbd0bb9190eee6e59dd9eca
mmartofel <mmartofe@redhat.com>
2023-04-01 15:01:51 +0200

    initial with all filies

---
+++ run.sh
@@ -0,0 +1,4 @@
+mvn spring-boot:run
+
+# run Docker image build
+# mvn spring-boot:build-image
---
+++ src/test/java/com/acme/springbootbootstrapproject/SpringBootBootstrapProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.springbootbootstrapproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBootstrapProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
